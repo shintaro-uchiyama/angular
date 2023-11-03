6fb3606d238ed5816559c87bc453e86740c87112
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2023-11-03 13:56:52 +0900

    add image

--- front/src/app/fabric/fabric.component.ts
+++ front/src/app/fabric/fabric.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-fabric',
   templateUrl: './fabric.component.html',
-  styleUrls: ['./fabric.component.scss']
+  styleUrls: ['./fabric.component.scss'],
 })
-export class FabricComponent {
+export class FabricComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('story', { static: true })
+  storyRef: ElementRef<HTMLCanvasElement> | undefined;
+  canvas: fabric.Canvas | undefined;
 
+  private imageUrls: Set<string> = new Set();
+
+  ngAfterViewInit(): void {
+    if (!this.storyRef) {
+      return;
+    }
+
+    this.canvas = new fabric.Canvas(this.storyRef.nativeElement, {
+      selection: true,
+      stateful: true,
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.imageUrls.forEach((url) => {
+      URL.revokeObjectURL(url);
+    });
+  }
+
+  /**
+   * ファイル選択時
+   * @param event
+   */
+  onFileSelected(event: Event) {
+    if (event.target === null) {
+      console.error('File upload target not found');
+      return;
+    }
+
+    const element = event.target as HTMLInputElement;
+    if (element.files === null || element.files.length === 0) {
+      console.error('File not found');
+      return;
+    }
+
+    const file: File = element.files[0];
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      this.imageUrls.add(imageUrl);
+
+      fabric.Image.fromURL(imageUrl, (img) => {
+        if (!this.canvas) {
+          return;
+        }
+
+        img.scaleToWidth(200); // 画像の幅を調整
+        img.scaleToHeight(200); // 画像の高さを調整
+        this.canvas.add(img);
+      });
+    }
+  }
 }
