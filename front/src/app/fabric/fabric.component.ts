ab7d34d20c92d580ed6cd0c57c95ab3148afad34
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2023-11-05 15:59:36 +0900

    limit select range moce

--- front/src/app/fabric/fabric.component.ts
+++ front/src/app/fabric/fabric.component.ts
@@ -20,6 +20,18 @@ export class FabricComponent implements AfterViewInit, OnDestroy {
 
   private imageUrls: Set<string> = new Set();
 
+  get oneObjectIsSelected(): boolean {
+    if (!this.canvas) {
+      return false;
+    }
+    return this.canvas.getActiveObjects().length === 1;
+  }
+
+  isSelectingRange: boolean = false;
+
+  private boundingBox: fabric.Object | undefined;
+  private movingBox: fabric.Rect | undefined;
+
   ngAfterViewInit(): void {
     if (!this.storyRef) {
       return;
@@ -28,6 +40,60 @@ export class FabricComponent implements AfterViewInit, OnDestroy {
     this.canvas = new fabric.Canvas(this.storyRef.nativeElement, {
       selection: true,
       stateful: true,
+    });
+
+    // this.canvas.on('selection:created', (event) => {
+    //   console.log('selected', event);
+    // });
+
+    this.canvas.on('object:moving', () => {
+      if (
+        this.movingBox === undefined ||
+        this.movingBox.top === undefined ||
+        this.movingBox.left === undefined ||
+        this.movingBox.width === undefined ||
+        this.movingBox.height === undefined ||
+        this.movingBox.scaleX === undefined ||
+        this.movingBox.scaleY === undefined ||
+        this.boundingBox === undefined ||
+        this.boundingBox.top === undefined ||
+        this.boundingBox.left === undefined ||
+        this.boundingBox.width === undefined ||
+        this.boundingBox.height === undefined ||
+        this.boundingBox.scaleX === undefined ||
+        this.boundingBox.scaleY === undefined
+      ) {
+        return;
+      }
+
+      const leftMaxLimit =
+        this.boundingBox.left +
+        this.boundingBox.width * this.boundingBox.scaleX -
+        this.movingBox.width * this.movingBox.scaleX;
+      const leftMinLimit = this.boundingBox.left;
+
+      let targetLeft = this.movingBox.left;
+
+      if (this.movingBox.left > leftMaxLimit) {
+        targetLeft = leftMaxLimit;
+      } else if (this.movingBox.left < leftMinLimit) {
+        targetLeft = leftMinLimit;
+      }
+
+      this.movingBox.left = targetLeft;
+
+      let targetTop = this.movingBox.top;
+      const topMaxLimit =
+        this.boundingBox.top +
+        this.boundingBox.height * this.boundingBox.scaleY -
+        this.movingBox.height * this.movingBox.scaleY;
+      const topMinLimit = this.boundingBox.top;
+      if (this.movingBox.top > topMaxLimit) {
+        targetTop = topMaxLimit;
+      } else if (this.movingBox.top < topMinLimit) {
+        targetTop = topMinLimit;
+      }
+      this.movingBox.top = targetTop;
     });
   }
 
@@ -69,5 +135,34 @@ export class FabricComponent implements AfterViewInit, OnDestroy {
         this.canvas.add(img);
       });
     }
+  }
+
+  onClickSelectRange(_event: MouseEvent) {
+    this.isSelectingRange = true;
+
+    const selectedObject = this.canvas?.getActiveObject();
+    if (!selectedObject) {
+      console.error('Selected object not found');
+      return;
+    }
+    console.log(selectedObject);
+    this.boundingBox = selectedObject;
+
+    const rect = new fabric.Rect({
+      top: selectedObject.top,
+      left: selectedObject.left,
+      fill: 'red',
+      width: (selectedObject.width ?? 300) * (selectedObject.scaleX ?? 1),
+      height: (selectedObject.height ?? 300) * (selectedObject.scaleY ?? 1),
+      opacity: 0.5,
+    });
+    this.movingBox = rect;
+
+    if (!this.canvas) {
+      console.error('Canvas not found');
+      return;
+    }
+    this.canvas.add(rect);
+    this.canvas.setActiveObject(rect);
   }
 }
